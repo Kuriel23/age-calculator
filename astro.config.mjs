d0ba63cddf1985c3e54b9c61d871c5e6602df396
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2024-10-04 21:39:50 +0100

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,11 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
